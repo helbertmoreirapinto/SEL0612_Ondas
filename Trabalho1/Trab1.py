c6d549a097277ba0b072c8c793c998232bf7b2a7
Helbert <helbert.tec.info@gmail.com>
2020-05-20 01:02:42 -0300

    ok

--- Trabalho1/Trab1.py
+++ Trabalho1/Trab1.py
@@ -11,10 +11,6 @@ def source2(t):
     k = t - (l/(9*sci.speed_of_light))
     return unit_step(t) - unit_step(k)
 
-def source3(t):
-    omega = 2 * math.pi
-    return 5*math.cos(omega*t)
-
 def unit_step(t):
     return 1 if t >= 0 else 0
 
@@ -23,22 +19,22 @@ def anim(n):
     ln2.set_ydata(V[n,:])
     return ln1, ln2,
 
-l = 50000                   #line size 30Km
+l = 70000                   #line size 70Km
 u = 0.9*sci.speed_of_light  #0.9c
 Z0 = 50                     #line impedancy
 Zg = 75                     #source impedancy
-Zc = 50               #charge impedancy
+Zc = 100                    #charge impedancy
 
 C = 1/(Z0*u)                #capacitor
 L = Z0/u                    #inducer
 
-hit_value = 10              #number of reflections
+hits = 10                   #number of reflections
 Nz = 100                    #number of space partitions
 dz = l/Nz
-dt = dz/u
-dt = 0.7*dt                 #stability condition
-
-T = (hit_value+0.5)*(l/u)   #total time
+alfa = 0.8                  #stability constant
+dt = alfa*dz/u              #stability condition
+        
+T = (hits+0.5)*(l/u)   #total time
 M = (int)(T/dt)             #number of time partitions
 
 V = np.zeros((M, Nz))       #voltage matrix
@@ -71,7 +67,7 @@ else:
 
 for n in range(0, M):
     if n == 0: #initial conditions
-        I[0, 0] = ( 1/(Zg+Z0))*source1(n)
+        I[0, 0] = source1(n)/(Zg+Z0)
         V[0, 0] = Z0*I[0, 0]
     elif n > 0:
         #border conditions
@@ -94,24 +90,24 @@ print('gama charge', gama2)
 
 lim_y = 0
 for i in ((int)(M/20), M-1):
-    aux1 = max(V[i], key=abs)
-    aux2 = max(I[i], key=abs)
+    aux1 = max(V[i,:], key=abs)
+    aux2 = max(I[i,:], key=abs)
     if aux1 > lim_y:
         lim_y = aux1
     if aux2 > lim_y:
         lim_y = aux2
-lim_y += 0.05*lim_y
+lim_y += 0.5*lim_y
 
 dlim_x = l
 dlim_x = 0.01*dlim_x
 
-fig1, ax1 = plt.subplots()
-ln1, = ax1.plot(xi, I[0], color='xkcd:blue', label='Current [A]')
-ln2, = ax1.plot(xv, V[0], color='xkcd:red', label='Voltage [V]')
+fig, ax = plt.subplots()
+ln1, = ax.plot(xi, I[0], color='xkcd:blue', label='Current [A]')
+ln2, = ax.plot(xv, V[0], color='xkcd:red', label='Voltage [V]')
 plt.xlabel('Distance [m]')
 plt.grid(True)
 plt.ylim(-lim_y, lim_y)
 plt.xlim(-dlim_x, l+dlim_x)
 plt.legend()
-anim = FuncAnimation(fig1, func=anim, frames=np.arange(0, M), interval = 100,blit = True)
+anim = FuncAnimation(fig, func=anim, frames=np.arange(0, M), interval = 100,blit = True)
 plt.show()
