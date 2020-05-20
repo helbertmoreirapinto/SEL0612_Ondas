67abbba67b63f258be8ab8718ccdf5a588525aba
Helbert <helbert.tec.info@gmail.com>
2020-05-19 22:23:14 -0300

    bug fix BC

--- Trabalho1/Trab1.py
+++ Trabalho1/Trab1.py
@@ -4,29 +4,30 @@ import scipy.constants as sci
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 
-def source_1(t):
+def source1(t):
     return 2 * unit_step(t)
 
-def source_2(t, l):
+def source2(t):
     k = t - (l/(9*sci.speed_of_light))
     return unit_step(t) - unit_step(k)
+
+def source3(t):
+    omega = 2 * math.pi
+    return 5*math.cos(omega*t)
 
 def unit_step(t):
     return 1 if t >= 0 else 0
 
-def anim_v(n):
-    ln2.set_ydata(V[n,:])
-    return ln2,
-
-def anim_i(n):
+def anim(n):
     ln1.set_ydata(I[n,:])
-    return ln1,
+    ln2.set_ydata(V[n,:])
+    return ln1, ln2,
 
-l = 30000                   #30Km
+l = 30000                   #line size 30Km
 u = 0.9*sci.speed_of_light  #0.9c
 Z0 = 50                     #line impedancy
 Zg = 75                     #source impedancy
-Zc = 000                    #charge impedancy
+Zc = math.inf               #charge impedancy
 
 C = 1/(Z0*u)                #capacitor
 L = Z0/u                    #inducer
@@ -48,47 +49,58 @@ xi = np.linspace(0, l, Nz-1)
 #constants
 tz = dt/dz
 beta1 = 2*tz/(Zg*C)
-beta2 = 2*tz/(Zc*C) if Zc != 0 else math.inf
+beta2 = 0
+beta3 = 0
 r = pow(tz, 2)/(L*C)
+short_circuit = False
+open_circuit = False
+
+if Zc == 0:
+    beta2 = math.inf
+    beta3 = math.inf
+    short_circuit = True
+elif Zc == math.inf:
+    beta2 = 0
+    beta3 = 0
+    open_circuit = True
+else:
+    beta2 = 2*tz/(Zc*C)
+    beta3 = tz/(Zc*C)
 
 for n in range(0, M):
     if n == 0: #initial conditions
-        I[0, 0] = ( 1/(Zg+Z0))*source_1(n)
+        I[0, 0] = ( 1/(Zg+Z0))*source3(n)
         V[0, 0] = Z0*I[0, 0]
 
     elif n > 0:
         #border conditions
-        V[n, 0] = (1-beta1)*V[n-1, 0] - 2*I[n-1, 0] + (2/Zg)*source_1(dt*(n-1))
-        V[n, Nz-1] = (1-beta2)*V[n-1, Nz-1] + 2*I[n-1, Nz-2]
-        I[n, Nz-2] = tz*V[n, Nz-1]/(C*Zc)
+        V[n, 0] = (1-beta1)*V[n-1, 0] - 2*I[n-1, 0] + (2/Zg)*source3(dt*(n-1))
+        if open_circuit:
+            V[n, Nz-1] = V[n, Nz-2]
+        elif short_circuit:
+            V[n, Nz-1] = 0
+        else:
+            V[n, Nz-1] = (1-beta2)*V[n-1, Nz-1] + 2*I[n-1, Nz-2]
         
         #Voltage
-        for k in range (1, Nz-2):
+        for k in range (1, Nz-1):
             V[n, k] = V[n-1, k] - (I[n-1, k] - I[n-1, k-1])
         
         #Current
-        for k in range (0, Nz-2):
+        for k in range (0, Nz-1):
             I[n, k] = I[n-1, k] - r*(V[n, k+1] - V[n, k])
 #V = tz*V/C
-fig2, ax2 = plt.subplots()
-ln2, = ax2.plot(xv, V[0])
-plt.grid(True)
-plt.ylim(-0.3, 0.3)
-plt.xlim(-10, l+10)
-plt.title('Voltage')
-anim2 = FuncAnimation(fig2, func=anim_v, frames=np.arange(0, M), interval = 100,blit = True)
+gama1 = (Z0-Zg)/(Z0+Zg)
+gama2 = (Z0-Zc)/(Z0+Zc)
+print('Coef reflexao source ', gama1)
+print('Coef reflexao charge ', gama2)
 
 fig1, ax1 = plt.subplots()
-ln1, = ax1.plot(xi, I[0])
+ln1, = ax1.plot(xi, I[0], color='xkcd:blue', label='Current')
+ln2, = ax1.plot(xv, V[0], color='xkcd:orange', label='Voltage')
 plt.grid(True)
-plt.ylim(-0.1, 0.1)
-plt.xlim(-10, l+10)
-plt.title('Current')
-anim1 = FuncAnimation(fig1, func=anim_i, frames=np.arange(0, M), interval = 100,blit = True)
-
+plt.ylim(-0.2, 0.2)
+plt.xlim(-100, l+100)
+plt.legend()
+anim = FuncAnimation(fig1, func=anim, frames=np.arange(0, M), interval = 100,blit = True)
 plt.show()
-
-'''
-if __name__ == '__main__':
-    main()
-'''
