e868f27a6dd74cadc09cdf04fb2a3e4b8ebb86e8
Helbert <helbert.tec.info@gmail.com>
2020-05-19 23:23:37 -0300

    bug fix Zc = 0

--- Trabalho1/Trab1.py
+++ Trabalho1/Trab1.py
@@ -23,11 +23,11 @@ def anim(n):
     ln2.set_ydata(V[n,:])
     return ln1, ln2,
 
-l = 30000                   #line size 30Km
+l = 50000                   #line size 30Km
 u = 0.9*sci.speed_of_light  #0.9c
 Z0 = 50                     #line impedancy
 Zg = 75                     #source impedancy
-Zc = math.inf               #charge impedancy
+Zc = 50               #charge impedancy
 
 C = 1/(Z0*u)                #capacitor
 L = Z0/u                    #inducer
@@ -36,7 +36,7 @@ hit_value = 10              #number of reflections
 Nz = 100                    #number of space partitions
 dz = l/Nz
 dt = dz/u
-dt = 0.5*dt                 #stability condition
+dt = 0.7*dt                 #stability condition
 
 T = (hit_value+0.5)*(l/u)   #total time
 M = (int)(T/dt)             #number of time partitions
@@ -53,31 +53,30 @@ beta2 = 0
 beta3 = 0
 r = pow(tz, 2)/(L*C)
 short_circuit = False
-open_circuit = False
 
+gama1 = (Zg-Z0)/(Zg+Z0)
 if Zc == 0:
     beta2 = math.inf
     beta3 = math.inf
+    gama2 = 1.0
     short_circuit = True
 elif Zc == math.inf:
-    beta2 = 0
-    beta3 = 0
-    open_circuit = True
+    beta2 = 0.0
+    beta3 = 0.0
+    gama2 = -1.0
 else:
     beta2 = 2*tz/(Zc*C)
     beta3 = tz/(Zc*C)
+    gama2 = (Zc-Z0)/(Zc+Z0)
 
 for n in range(0, M):
     if n == 0: #initial conditions
-        I[0, 0] = ( 1/(Zg+Z0))*source3(n)
+        I[0, 0] = ( 1/(Zg+Z0))*source1(n)
         V[0, 0] = Z0*I[0, 0]
-
     elif n > 0:
         #border conditions
-        V[n, 0] = (1-beta1)*V[n-1, 0] - 2*I[n-1, 0] + (2/Zg)*source3(dt*(n-1))
-        if open_circuit:
-            V[n, Nz-1] = V[n, Nz-2]
-        elif short_circuit:
+        V[n, 0] = (1-beta1)*V[n-1, 0] - 2*I[n-1, 0] + (2/Zg)*source1(dt*(n-1))
+        if short_circuit:
             V[n, Nz-1] = 0
         else:
             V[n, Nz-1] = (1-beta2)*V[n-1, Nz-1] + 2*I[n-1, Nz-2]
@@ -89,18 +88,30 @@ for n in range(0, M):
         #Current
         for k in range (0, Nz-1):
             I[n, k] = I[n-1, k] - r*(V[n, k+1] - V[n, k])
-#V = tz*V/C
-gama1 = (Z0-Zg)/(Z0+Zg)
-gama2 = (Z0-Zc)/(Z0+Zc)
-print('Coef reflexao source ', gama1)
-print('Coef reflexao charge ', gama2)
+
+print('gama source', gama1)
+print('gama charge', gama2)
+
+lim_y = 0
+for i in ((int)(M/20), M-1):
+    aux1 = max(V[i], key=abs)
+    aux2 = max(I[i], key=abs)
+    if aux1 > lim_y:
+        lim_y = aux1
+    if aux2 > lim_y:
+        lim_y = aux2
+lim_y += 0.05*lim_y
+
+dlim_x = l
+dlim_x = 0.01*dlim_x
 
 fig1, ax1 = plt.subplots()
-ln1, = ax1.plot(xi, I[0], color='xkcd:blue', label='Current')
-ln2, = ax1.plot(xv, V[0], color='xkcd:orange', label='Voltage')
+ln1, = ax1.plot(xi, I[0], color='xkcd:blue', label='Current [A]')
+ln2, = ax1.plot(xv, V[0], color='xkcd:red', label='Voltage [V]')
+plt.xlabel('Distance [m]')
 plt.grid(True)
-plt.ylim(-0.2, 0.2)
-plt.xlim(-100, l+100)
+plt.ylim(-lim_y, lim_y)
+plt.xlim(-dlim_x, l+dlim_x)
 plt.legend()
 anim = FuncAnimation(fig1, func=anim, frames=np.arange(0, M), interval = 100,blit = True)
 plt.show()
